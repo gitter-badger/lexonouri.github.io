6100b44e66a90275076bb139854ad321e84c5e53
Mateusz Janota <lexonouri@gmail.com>
2015-04-02 12:01:14 +0200

    testing index view

--- hello/views.py
+++ hello/views.py
@@ -5,15 +5,9 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('../index.html')
+    return render('../index.html')
 
 
 def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
+    return render('../index.html', 'db.html')
 
